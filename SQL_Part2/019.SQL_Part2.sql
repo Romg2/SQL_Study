e4eda2438da3732a5d78be2109d81758dece9935
Romg2 <ekzm330@naver.com>
2021-04-13 18:18:38 +0900

    Part2-019

--- SQL_Part2/019.SQL_Part2.sql
+++ SQL_Part2/019.SQL_Part2.sql
@@ -1,3 +1,7 @@
 -- 019. 문자에서 특정 철자의 위치 출력하기
-SELECT INSTR('Romg2', 'o')
+SELECT INSTR('Romg2', 'o') AS VAR1
+FROM DUAL;
+
+-- 응용 SUBSTR, INSTR
+SELECT SUBSTR('abcdef@naver.com', INSTR('abcdef@naver.com', '@') + 1, LENGTH('naver.com') - 4) AS VAR1
 FROM DUAL;
