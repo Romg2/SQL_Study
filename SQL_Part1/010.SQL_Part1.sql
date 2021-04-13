9dac512e2365c01d3a2012fd8bf8402861e6dbea
Romg2 <ekzm330@naver.com>
2021-04-13 16:45:03 +0900

    Part1-010

---
+++ SQL_Part1/010.SQL_Part1.sql
@@ -0,0 +1,10 @@
+-- 010. 비교 연산자 배우기
+-- >, <, >=, <=, =, !=
+SELECT ENAME, SAL, DEPTNO
+FROM EMP
+WHERE SAL > 1250
+--    SAL < 1250
+--    SAL >= 1250
+--    SAL <= 1250
+--    SAL = 1250
+--    SAL != 1250;
