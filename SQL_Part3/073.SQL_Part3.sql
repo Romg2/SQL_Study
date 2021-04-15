e6236e98f161f7420fbdb5f68ddceff7ebb65d78
Romg2 <ekzm330@naver.com>
2021-04-15 11:55:40 +0900

    Part3-073

---
+++ SQL_Part3/073.SQL_Part3.sql
@@ -0,0 +1,13 @@
+-- 72. 서브 쿼리 사용하기3
+
+-- 관리자가 아닌 사원 출력
+SELECT ENAME
+FROM EMP
+WHERE EMPNO NOT IN (SELECT MGR
+                    FROM EMP
+                    WHERE MGR IS NOT NULL);
+
+-- NOT IN 사용시 유의점
+-- IS NOT NULL을 사용하지 않으면 결과가 출력되지 않을 수 있다.
+-- Ex) MGR이 (111, 222, NULL)인 경우
+-- WHERE EMPNO != 111 AND EMPNO != 222 AND != NULL로 식이 만들어지며 이 경우 전체가 NULL이 된다.
