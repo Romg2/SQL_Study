11632e1a9c51639e699a9e113cd56f9693bdb2e6
Romg2 <ekzm330@naver.com>
2021-04-15 10:57:58 +0900

    Part3-064

---
+++ SQL_Part3/065.SQL_Part3.sql
@@ -0,0 +1,8 @@
+-- 64. 여러 테이블의 데이터를 조인해서 출력하기8
+
+-- LEFT/RIGHT OUTER JOIN
+-- OUTER JOIN (+) 붙이는 것과 같은 결과
+-- 데이터가 많은 쪽을 출력
+SELECT B.DEPTNO, B.ENAME, A.LOC
+FROM DEPT A
+LEFT OUTER JOIN EMP B ON A.DEPTNO = B.DEPTNO;
