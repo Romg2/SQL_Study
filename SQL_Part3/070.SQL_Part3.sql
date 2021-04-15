3fd504996cd4aeb664ca5f9a01a63cc84a6a421d
Romg2 <ekzm330@naver.com>
2021-04-15 11:29:10 +0900

    Part3-070

---
+++ SQL_Part3/070.SQL_Part3.sql
@@ -0,0 +1,14 @@
+-- 70. 집합 연산자로 데이터의 차집합을 출력하기
+
+-- MINUS
+-- UNION과 마찬가지로 중복된 데이터가 있다면 제거 / 오름차순 정렬
+-- 중복의 기준은 "행 단위"
+SELECT ENAME, SAL, JOB, DEPTNO
+FROM EMP
+WHERE DEPTNO IN (10, 20)
+
+MINUS
+
+SELECT ENAME, SAL, JOB, DEPTNO
+FROM EMP
+WHERE DEPTNO IN (20, 30);
